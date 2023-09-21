d4c4794dacf3511f343eb3baf54bbefcbcb93d5a
SteveArms <acunoc@unsa.edu.pe>
2023-09-20 21:38:41 -0500

    Laboratorio 1 - Ejercicio 2

---
+++ Ejercicio2L1.java
@@ -0,0 +1,34 @@
+//Laboratorio 1 : Ejercicio 2
+//Autor: Cuno Cahuari Armando Steven
+//Tiempo:
+import java.util.*;
+public class Ejercicio2L1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del 1er soldado :");
+        String s1  = sc.next();
+        System.out.println("Ingrese el nivel de vida del 1er soldado :");
+        String v1 = sc.next();
+        System.out.println("Ingrese el nombre del 2do soldado :");
+        String s2  = sc.next();
+        System.out.println("Ingrese el nivel de vida del 2do soldado :");
+        String v2 = sc.next();
+        System.out.println("Ingrese el nombre del 3er soldado :");
+        String s3  = sc.next();
+        System.out.println("Ingrese el nivel de vida del 3er soldado :");
+        String v3 = sc.next();
+        System.out.println("Ingrese el nombre del 4to soldado :");
+        String s4  = sc.next();
+        System.out.println("Ingrese el nivel de vida del 4to soldado :");
+        String v4 = sc.next();
+        System.out.println("Ingrese el nombre del 5to soldado :");
+        String s5  = sc.next();
+        System.out.println("Ingrese el nivel de vida del 5to soldado :");
+        String v5 = sc.next();
+        System.out.println("El 1er soldado se llama : " + s1 + " con nivel de vida : " + v1);
+        System.out.println("El 2do soldado se llama : " + s2 + " con nivel de vida : " + v2);
+        System.out.println("El 3er soldado se llama : " + s3 + " con nivel de vida : " + v3);
+        System.out.println("El 4to soldado se llama : " + s4 + " con nivel de vida : " + v4);
+        System.out.println("El 5to soldado se llama : " + s5 + " con nivel de vida : " + v5);
+    }
+}
