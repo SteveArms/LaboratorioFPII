4a7d2e3eb2122ab5cef43055bff6dfcc566113cf
SteveArms <acunoc@unsa.edu.pe>
2023-09-20 21:33:39 -0500

    Laboratorio 1 - Ejercicio 1

---
+++ Ejercicio01.java
@@ -0,0 +1,26 @@
+// Laboratorio Nro 1 : Ejercicio 1
+// Autor : Cuno Cahuari Armando Steven
+// Tiempo:
+import java.util.*;
+class Ejercicio01{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del 1er soldado :");
+        String s1 = sc.next();
+        System.out.println("Ingrese el nombre del 2do soldado :");
+        String s2 = sc.next();
+        System.out.println("Ingrese el nombre del 3er soldado :");
+        String s3 = sc.next();
+        System.out.println("Ingrese el nombre del 4to soldado :");
+        String s4 = sc.next();
+        System.out.println("Ingrese el nombre del 5to soldado :");
+        String s5 = sc.next();
+        System.out.println("El primer soldado es : " + s1);
+        System.out.println("El segundo soldado es : " + s2);
+        System.out.println("El tercero soldado es : " + s3);
+        System.out.println("El cuarto soldado es : " + s4);
+        System.out.println("El quinto soldado es : " + s5);
+
+
+    }
+}    
